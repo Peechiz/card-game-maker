065f33a42d4843297d9a1476245156b45e225973
Chris Impicciche <cimpicci@gmail.com>
2018-09-20 21:01:38 -0500

    fixes card/pool load, adds img handling

--- js/deckMaker.js
+++ js/deckMaker.js
@@ -1,3 +1,5 @@
+let store = firebase.storage();
+
 let app = new Vue({
   el: '#app',
   data: {
@@ -7,67 +9,23 @@ let app = new Vue({
     },
     decks: [],
     pool: {},
-    cards: {}
+    cards: {},
+    addingDeck: false,
+    newDeckName: ''
   },
   created(){
-
-    // GET DECKS
-    decksRef.once('value').then( res => {
-      // console.log('decks:', res.val())
-      let currDecks = res.val()
-
-      res.forEach(deck => {
-        this.decks.push({
-          name: deck.val().name,
-          key: deck.key
-        })
-      })
-
-    }, err => {
-      console.err(err)
-    })
-
-    cardsRef.once('value').then(res => {
-      res.forEach(card => {
-
-        this.cards[card.key] = {
-          num: 1,
-          added: false,
-          ...card.val()
-        }
-
-      })
-    })
-
+    this.loadDecks();
+    this.loadCards();
   },
   methods: {
     select(){
       // if a deck exists, populate it's current card pool
-      if (this.selectedDeck.name){
-        console.log('get card pool for that deck')
-        const key = this.selectedDeck.key
-        decksRef.child(key + '/cards')
-          .once('value')
-          .then(res => {
-            res.forEach(card => {
-
-              const key = card.val().key;
-
-              // toggle viewable state for cards already in deck
-              this.toggleAdded(card.val().key)
-              
-              // add to pool UI
-              this.$set(this.pool, key, this.cards[key])
-              this.pool[key].num = card.val().num
-              this.pool[key].self = card.key;
-
-            })
-          })
-      }
+      this.loadCards();
+      this.loadPool();
     },
     addToPool(card, ref){
-      console.log('adding card to pool', card);
-      console.log('addref', ref)
+      // console.log('adding card to pool', card);
+      // console.log('addref', ref)
       const key = this.selectedDeck.key
 
       let cardRef = decksRef.child(key+'/cards').push({
@@ -104,7 +62,7 @@ let app = new Vue({
     remove(card, key){
       const deck = this.selectedDeck.key
       // remove card from db deck
-      decksRef.child(deck + '/cards/' + card.self).remove()
+      decksRef.ref('/cards').child(deck + '/cards/' + card.self).remove()
       
       // toggle [cards] view state
       this.cards[key].added = false;
@@ -113,5 +71,86 @@ let app = new Vue({
       // remove card from pool
       this.$delete(this.pool, key)
     },
+    loadCards(){
+      // console.log('loading cards...')
+      cardsRef.once('value').then(res => {
+        res.forEach(card => {
+          const cv = card.val();
+
+          const setCard = () => {
+            this.$set(this.cards, card.key, {
+              num: 1,
+              added: false,
+              ...cv
+            })
+          }
+
+          if (cv.img){
+            // console.log('loading img...', cv.img)
+            store.ref('/cards').child(cv.img).getDownloadURL().then(url => {
+              setCard();
+              this.cards[card.key].img = url;
+            })
+          } else {
+            setCard();
+          }
+
+        })
+      })
+    },
+    loadDecks(){
+      decksRef.once('value').then( res => {
+        // console.log('decks:', res.val())
+        let currDecks = res.val()
+  
+        res.forEach(deck => {
+          this.decks.push({
+            name: deck.val().name,
+            key: deck.key
+          })
+        })
+  
+      }, err => {
+        console.err(err)
+      })
+    },
+    loadPool(){
+      this.pool = {};
+      console.log('get card pool for selected deck');
+      const key = this.selectedDeck.key;
+      decksRef.child(key + '/cards')
+        .once('value')
+        .then(res => {
+          res.forEach(card => {
+
+            const key = card.val().key;
+
+            // toggle viewable state for cards already in deck
+            this.toggleAdded(card.val().key)
+            
+            // add to pool UI
+            this.$set(this.pool, key, this.cards[key])
+            this.pool[key].num = card.val().num
+            this.pool[key].self = card.key;
+
+          })
+        })
+        .catch(err => {
+          console.log('loadpool err', err);
+        })
+    },
+    submitDeck(){
+      // console.log('new deck!', this.newDeckName)
+      let deckRef = decksRef.push();
+      deckRef.set({name: this.newDeckName})
+      this.decks.push({
+        name: this.newDeckName,
+        key: deckRef.key
+      })
+      this.toggleAddDeck();
+    },
+    toggleAddDeck(){
+      this.addingDeck = !this.addingDeck;
+    }
   }
 })
